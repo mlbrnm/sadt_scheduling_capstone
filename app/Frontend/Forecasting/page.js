29112f4d82d99aef37d0184a05ef9b0545409d5c
Devyn Weir <devynweir@gmail.com>
2025-09-05 16:11:53 -0600

    Initial Forecasting Page Construction

---
+++ app/Frontend/Forecasting/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Forecasting() {
+  return (
+    <div class="flex justify-center space-x-40">
+      <h1>Academic Year</h1>
+      <h1>Semester</h1>
+      <h1>Program</h1>
+    </div>
+  );
+}
