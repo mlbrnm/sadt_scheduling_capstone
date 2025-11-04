27143c5f3f8d93034cd832925e00d2d40581b758
Aariyana Sayani <arisayani@gmail.com>
2025-11-03 19:12:02 -0700

    individual; instructor profile created page

---
+++ app/Frontend/InstructorProfiles/IndividualProfile/page.js
@@ -0,0 +1,11 @@
+'use client';
+import { useState, useEffect } from "react";
+import { supabase } from "../../supabaseClient";
+
+export default function IndivProfile() {
+    return (
+        <div>
+            <h1>Instructor Individual Profile Page</h1>
+        </div>
+    );
+}
