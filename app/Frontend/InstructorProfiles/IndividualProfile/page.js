68b3aca81acb3ddabd2a351103d976777311b308
Amrit Reddy <amrit_reddy@me.com>
2025-11-30 13:50:40 -0700

    IndividualProfile update
    Fix loading spinner styling.
    Fix padding in Add New Instructor modal.
    Fix buttons in Add New Instructor modal.

--- app/Frontend/InstructorProfiles/IndividualProfile/page.js
+++ app/Frontend/InstructorProfiles/IndividualProfile/page.js
@@ -1,148 +1,185 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
 import { supabase } from "../../supabaseClient";
-import { useRouter, useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
 
 export default function IndivProfile() {
-    const [instructorData, setInstructorData] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const searchParams = useSearchParams();
-    const instructorId = searchParams.get("id");
+  const [instructorData, setInstructorData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const searchParams = useSearchParams();
+  const instructorId = searchParams.get("id");
 
+  // Fetch instructor data from database based on instructorId (pk)
+  useEffect(() => {
+    const fetchInstructorData = async () => {
+      if (!instructorId) return;
+      try {
+        setIsLoading(true);
+        const { data, error } = await supabase
+          .from("instructors")
+          .select("*")
+          .eq("instructor_id", instructorId)
+          .single();
 
-    // Fetch instructor data from database based on instructorId (pk)
-    useEffect(() => {
-        const fetchInstructorData = async () => {
-            if (!instructorId) return;
-            try {
-                setIsLoading(true);
-                const { data, error } = await supabase
-                    .from("instructors")
-                    .select("*")
-                    .eq("instructor_id", instructorId)
-                    .single();
-
-                if (error) {
-                    console.error("Error fetching instructor data:", error);
-                    setInstructorData(null);
-                } else {
-                    console.log("Instructor data:", data);
-                    setInstructorData(data);
-                }
-                setIsLoading(false);
-            } catch (error) {
-                console.error("Unexpected error:", error);
-                setIsLoading(false);
-            }
-        };
-
-        if (instructorId) {
-            fetchInstructorData();
+        if (error) {
+          console.error("Error fetching instructor data:", error);
+          setInstructorData(null);
+        } else {
+          console.log("Instructor data:", data);
+          setInstructorData(data);
         }
-    }, [instructorId]);
-
-    if (isLoading) {
-        return (
-            <div className="p-8 spinner rounded-full border-t-2 border-b border-gray-900">Loading...</div>
-        );
-    }
-
-    // Status colour conditional rendering
-    const getStatusColor = (status) => {
-        switch (status) {
-            case "Active":
-                return "chip-active px-4 py-1 w-20 text-center rounded border";
-            case "Renew":
-                return "chip-renew px-4 py-1 w-20 text-center rounded border";
-            case "Inactive":
-                return "chip-inactive px-4 py-1 w-20 text-center rounded border";
-            case "Expire":
-                return "chip-expire px-4 py-1 w-20 text-center rounded border";
-            case "On Leave":
-                return "chip-onleave px-4 py-1 w-20 text-center rounded border";
-            default:
-                return "chip-blank px-4 py-1 w-20 text-center rounded border";
-        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Unexpected error:", error);
+        setIsLoading(false);
+      }
     };
 
+    if (instructorId) {
+      fetchInstructorData();
+    }
+  }, [instructorId]);
+
+  if (isLoading) {
     return (
-        /* MAIN CONTENT CONTAINER */
-        <div className="p-8">
-            {/* Top Half of Page Container */}
-            <div className="flex mb-4">
-                {/* Profile Pic */}
-                <div className="flex mb-4 border border-blue h-64 w-64 rounded-full items-center justify-center bg-gray-400 mr-10 mt-2 ml-12">
-                    <Image src="/default-avatar.jpg" alt={`${instructorData?.instructor_name} ${instructorData?.instructor_lastname}`} width={275} height={300} className="rounded-full object-cover" />
-                </div>
-                {/* Basic Instructor Info */}
-                <div className="mt-6 mx-9">
-                    <span className="text-5xl font-semibold">{instructorData?.instructor_name} {instructorData?.instructor_lastname}</span>
-                    <div className="flex items-center mt-5 ml-2">
-                        <span className="text-lg mr-10">ID: {instructorData?.instructor_id}</span>
-                        <span className={`text-lg ${getStatusColor(instructorData?.instructor_status)}`}>{instructorData?.instructor_status || "-"}</span>
-                        <span className="text-lg ml-6">Since: {instructorData?.salaried_begin_date || "-"}</span>
-                    </div>
-                    <div className=" mt-4 ml-2">
-                        <span className="text-lg chip-contract-type border px-4 py-2 w-30 text-center rounded">{instructorData?.contract_type || "-"}</span>
-                        <span className="text-lg ml-6">CCH Target: {instructorData?.cch_target_ay2025 || "-"}</span>
-                    </div>
-                    <div className="text-lg mt-4 ml-2">
-                        <span className="text-lg">Reports To: {instructorData?.reporting_ac}</span>
-                        <span className="text-lg ml-6">End Date: {instructorData?.contract_end || "-"}</span>
-                    </div>
-                </div>
-                <div className="mt-12 mx-12">
-                    <ul>
-                        <li className="text-xl ml-64 text-sait-red font-medium italic underline">{instructorData?.instructor_name}.{instructorData?.instructor_lastname}@sait.ca</li>
-                        <li className="text-lg ml-64 mt-3">Office: NN406</li>
-                        <li className="text-lg ml-64 mt-3">Phone: 403-555-1234 (ext. 9)</li>
-                    </ul>
-                </div>
-            </div>
-            {/* Bottom Half of Page Container */}
-            <div className="flex mt-10 mx-14 gap-48 justify-center">
-                {/* Current Courses */}
-                <div>
-                    <span className="font-semibold text-xl">Current Courses</span>
-                    <ul className="list-disc m-4">
-                        <li className="text-lg mt-1">CPRG-211 A</li>
-                        <li className="text-lg mt-1">CPRG-211 B</li>
-                        <li className="text-lg mt-1">CPRG-304 B</li>
-                        <li className="text-lg mt-1">CPRG-304 D</li>
-                    </ul>
-                    <span className="font-semibold text-xl ml-5">Total: 4</span>
-                </div>
-                {/* Eligible Courses */}
-                <div>
-                    <span className="font-semibold text-xl">Eligible Courses</span>
-                    <ul className="list-disc m-4">
-                        <li className="text-lg mt-1">CPRG-211</li>
-                        <li className="text-lg mt-1">CPRG-216</li>
-                        <li className="text-lg mt-1">CPRG-304</li>
-                        <li className="text-lg mt-1">CPSY 202</li>
-                        <li className="text-lg mt-1">INTP-302</li>
-                    </ul>
-                </div>
-                {/* Previously Taught */}
-                <div>
-                    <span className="font-semibold text-xl">Previously Taught</span>
-                    <ul className="list-disc m-4">
-                        <li className="text-lg mt-1">CPRG-211</li>
-                        <li className="text-lg mt-1">CPRG-304</li>
-                        <li className="text-lg mt-1">INTP-302</li>
-                    </ul>
-                </div>
-                {/* Notes/Comments Section*/}
-                <div>
-                    <ul className="font-semibold text-xl">Notes/Comments:
-                        <li className="text-lg my-2 font-normal">Work from home thursdays</li>
-                        <li className="text-lg mt-4 mb-2 font-normal">PMP Certification in Progress</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+      <div className="flex justify-center items-center min-h-[80vh]">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-700"></div>
+        <span className="ml-3">Loading instructor...</span>
+      </div>
     );
+  }
+
+  // Status colour conditional rendering
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "Active":
+        return "chip-active px-4 py-1 w-20 text-center rounded border";
+      case "Renew":
+        return "chip-renew px-4 py-1 w-20 text-center rounded border";
+      case "Inactive":
+        return "chip-inactive px-4 py-1 w-20 text-center rounded border";
+      case "Expire":
+        return "chip-expire px-4 py-1 w-20 text-center rounded border";
+      case "On Leave":
+        return "chip-onleave px-4 py-1 w-20 text-center rounded border";
+      default:
+        return "chip-blank px-4 py-1 w-20 text-center rounded border";
+    }
+  };
+
+  return (
+    /* MAIN CONTENT CONTAINER */
+    <div className="p-8">
+      {/* Top Half of Page Container */}
+      <div className="flex mb-4">
+        {/* Profile Pic */}
+        <div className="flex mb-4 border border-blue h-64 w-64 rounded-full items-center justify-center bg-gray-400 mr-10 mt-2 ml-12">
+          <Image
+            src="/default-avatar.jpg"
+            alt={`${instructorData?.instructor_name} ${instructorData?.instructor_lastname}`}
+            width={275}
+            height={300}
+            className="rounded-full object-cover"
+          />
+        </div>
+        {/* Basic Instructor Info */}
+        <div className="mt-6 mx-9">
+          <span className="text-5xl font-semibold">
+            {instructorData?.instructor_name}{" "}
+            {instructorData?.instructor_lastname}
+          </span>
+          <div className="flex items-center mt-5 ml-2">
+            <span className="text-lg mr-10">
+              ID: {instructorData?.instructor_id}
+            </span>
+            <span
+              className={`text-lg ${getStatusColor(
+                instructorData?.instructor_status
+              )}`}
+            >
+              {instructorData?.instructor_status || "-"}
+            </span>
+            <span className="text-lg ml-6">
+              Since: {instructorData?.salaried_begin_date || "-"}
+            </span>
+          </div>
+          <div className=" mt-4 ml-2">
+            <span className="text-lg chip-contract-type border px-4 py-2 w-30 text-center rounded">
+              {instructorData?.contract_type || "-"}
+            </span>
+            <span className="text-lg ml-6">
+              CCH Target: {instructorData?.cch_target_ay2025 || "-"}
+            </span>
+          </div>
+          <div className="text-lg mt-4 ml-2">
+            <span className="text-lg">
+              Reports To: {instructorData?.reporting_ac}
+            </span>
+            <span className="text-lg ml-6">
+              End Date: {instructorData?.contract_end || "-"}
+            </span>
+          </div>
+        </div>
+        <div className="mt-12 mx-12">
+          <ul>
+            <li className="text-xl ml-64 text-sait-red font-medium italic underline">
+              {instructorData?.instructor_name}.
+              {instructorData?.instructor_lastname}@sait.ca
+            </li>
+            <li className="text-lg ml-64 mt-3">Office: NN406</li>
+            <li className="text-lg ml-64 mt-3">Phone: 403-555-1234 (ext. 9)</li>
+          </ul>
+        </div>
+      </div>
+      {/* Bottom Half of Page Container */}
+      <div className="flex mt-10 mx-14 gap-48 justify-center">
+        {/* Current Courses */}
+        <div>
+          <span className="font-semibold text-xl">Current Courses</span>
+          <ul className="list-disc m-4">
+            <li className="text-lg mt-1">CPRG-211 A</li>
+            <li className="text-lg mt-1">CPRG-211 B</li>
+            <li className="text-lg mt-1">CPRG-304 B</li>
+            <li className="text-lg mt-1">CPRG-304 D</li>
+          </ul>
+          <span className="font-semibold text-xl ml-5">Total: 4</span>
+        </div>
+        {/* Eligible Courses */}
+        <div>
+          <span className="font-semibold text-xl">Eligible Courses</span>
+          <ul className="list-disc m-4">
+            <li className="text-lg mt-1">CPRG-211</li>
+            <li className="text-lg mt-1">CPRG-216</li>
+            <li className="text-lg mt-1">CPRG-304</li>
+            <li className="text-lg mt-1">CPSY 202</li>
+            <li className="text-lg mt-1">INTP-302</li>
+          </ul>
+        </div>
+        {/* Previously Taught */}
+        <div>
+          <span className="font-semibold text-xl">Previously Taught</span>
+          <ul className="list-disc m-4">
+            <li className="text-lg mt-1">CPRG-211</li>
+            <li className="text-lg mt-1">CPRG-304</li>
+            <li className="text-lg mt-1">INTP-302</li>
+          </ul>
+        </div>
+        {/* Notes/Comments Section*/}
+        <div>
+          <ul className="font-semibold text-xl">
+            Notes/Comments:
+            <li className="text-lg my-2 font-normal">
+              Work from home thursdays
+            </li>
+            <li className="text-lg mt-4 mb-2 font-normal">
+              PMP Certification in Progress
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 /* first name, last name, id, cch target, contract type, instructor status, start date, end date, reporting ac */
--- app/Frontend/InstructorProfiles/page.js
+++ app/Frontend/InstructorProfiles/page.js
@@ -178,7 +178,7 @@ function AddInstructorModal({ onClose, onSuccess }) {
   // }
   return (
     <div className="fixed inset-0 bg-slate-150 bg-opacity-75 backdrop-blur-md flex items-center justify-center z-50 p-4">
-      <div className="bg-white rounded-lg shadow-sm max-w-2xl w-full max-h-[90vh] overflow-y-auto">
+      <div className="bg-white rounded-lg shadow-sm max-w-2xl w-full max-h-[90vh] overflow-y-auto p-4">
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-2 gap-4">
             <div>
@@ -320,14 +320,14 @@ function AddInstructorModal({ onClose, onSuccess }) {
             <button
               type="button"
               onClick={onClose}
-              className="button-secondary px-4 py-2 rounded-lg"
+              className="button-secondary px-4 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 cursor-pointer"
               disabled={isSubmitting}
             >
               Cancel
             </button>
             <button
               type="submit"
-              className="button-primary px-4 py-2 rounded-lg"
+              className="button-primary px-4 py-2 rounded-lg text-white cursor-pointer"
               disabled={isSubmitting}
             >
               {isSubmitting ? "Saving..." : "Save"}
@@ -412,7 +412,7 @@ export default function InstructorProfiles() {
   return (
     <div className="p-6 flex flex-col h-full overflow-hidden">
       {/* Title and Search Bar */}
-      <div className="flex justify-center items-center mb-4">
+      <div className="flex justify-end items-center mb-4">
         {/*Search Box */}
         <input
           type="text"
@@ -425,7 +425,7 @@ export default function InstructorProfiles() {
         {/*Add Instructor Button*/}
         <button
           onClick={() => setShowAddInstructorModal(true)}
-          className="ml-4 button-primary text-white px-4 py-2 rounded-lg cursor-pointer"
+          className="button-primary text-white px-4 py-2 rounded-lg cursor-pointer ml-80"
         >
           + Add New Instructor
         </button>
